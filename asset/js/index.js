d3dcf91c800b356fe01e6b30454a77f7cc1b8921
hello5236 <clemgc33@gmail.com>
2023-07-19 11:23:51 +0200

    ajout des animation au chargement de la page

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,2 +1,28 @@
 console.log("chargement terminer !")
 console.log("Site crée par royaly#0001, @royaly")
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries, observer) => {
+        for (const entry of entries) {
+            console.log(entries)
+            if (entry.isIntersecting) {
+                entry.target.animate([
+                    { transform: 'translateY(-10px)', opacity: 0 },
+                    { transform: 'translateY(0px)', opacity: 1 }
+                ], {
+                    duration: 800
+                })
+
+                observer.unobserve(entry.target)
+            }
+        }
+    })
+
+    observer.observe(document.querySelector('#view'))
+    observer.observe(document.querySelector('.home-img'))
+    observer.observe(document.querySelector('.view-competence'))
+    observer.observe(document.querySelector('.skill-bars'))
+    observer.observe(document.querySelector('.projet-texte'))
+    observer.observe(document.querySelector('.projet-pro'))
+    observer.observe(document.querySelector('.footer-text'))
+})
